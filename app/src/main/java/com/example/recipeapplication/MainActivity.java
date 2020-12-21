e49dc29231751941d3f7ab9e8754cff5627dc841
Shreyash <shreyash.pawar26@gmail.com>
2020-12-21 23:12:26 +0530

    Done with Splash Screen and started work on Homepage

--- app/src/main/java/com/example/recipeapplication/MainActivity.java
+++ app/src/main/java/com/example/recipeapplication/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.recipeapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,DashBoardActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },1600);
+
+
+
     }
 }
